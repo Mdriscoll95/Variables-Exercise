89eab682cbc1fc49c1328f57290fb83edc11f91b
Mdriscoll95 <michael.driscoll951@gmail.com>
2023-05-11 11:48:54 -0700

    HW Changes

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -2,9 +2,18 @@
 {
     public class Program
     {
+     
+
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+        { 
+        string myName = "Mike";
+        int myAge = 27;
+        char myFirstInitial = 'm';
+        bool mikeIsCoding = true;
+        double myWeight = 190.5;
+        decimal money = 650.00m;
+
+            Console.WriteLine($" hello my name is {myName}, my age is {myAge}, my first initial is {myFirstInitial}, i am coding {mikeIsCoding},  my weight is {myWeight}, in my pockey is have {money} ");
         }
     }
 }
